d905c9610c20519052ea8532ee0f05aec379fb46
Samuel Moraes <samuelm.moraes@hotmail.com>
2023-06-23 13:51:02 -0300

    modified:   script/script.js 	modified:   style/style.css 	renamed:    template/index.html -> template/cadastro.html 	new file:   template/login.html 	new file:   template/main.html

--- script/script.js
+++ script/script.js
@@ -19,3 +19,23 @@ function valida_cadastro_professor(event){
         alert(mensagem_confirmação)
     }
 }
+
+function realiza_login(event) {
+    var email = document.getElementById("email_login").value;
+    var senha = document.getElementById("senha_login").value;
+    var senha_padrao = 123
+
+    if(email == ''){
+        alert('Email invalido')
+    }
+
+    else{
+    if(email == 'samuel@escola.gov' && senha == senha_padrao){
+        alert('Logado com sucesso')
+    }
+
+    else{
+        alert('Usuario não cadastrado!')
+    }
+}
+}
